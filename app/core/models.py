4027a8c3f15e3ca1d8a0d6c33bc98913d598b036
ValyT <40295158+ValyT@users.noreply.github.com>
2023-08-23 11:47:36 +0300

    added user model functionality

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,31 @@
-from django.db import models # noqa
+""""
+Database models
+"""
+from django.db import models
+from django.contrib.auth.modles import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+    def create_user(self, email, password=None, **extra_field):
+        """Creates, save, return new user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self_db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in system"""
+    email=models.EmailField(max_length=255,unique=True)
+    name=models.CharField(max_length=255)
+    is_active=models.BooleanField(default=True)
+    is_staff=models.BooleanField(default=False)
+
+    object=UserManager()
+
+    USERNAME_FIELD='email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,13 +4,14 @@ Test for models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
     """Test models"""
     def test_create_user_with_email_successful(self):
         """Test creating a user with email        """
-        email='test@example.com'
-        password='testpass123'
-        user=get_user_model().objects.create_user(
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
             email=email,
             password=password
         )
