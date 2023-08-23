e39b8430c08cec9178f7959d7553b92f162f19c7
ValyT <40295158+ValyT@users.noreply.github.com>
2023-08-23 16:44:18 +0300

    bugs

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -4,13 +4,13 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializers):
+class UserSerializer(serializers.ModelSerializer):
     """Serializers for user object"""
 
     class Meta:
-        mode=get_user_model()
+        model=get_user_model()
         fields=['email','password','name']
-        extra_kwargs = {'password': {'write__only': True, 'min_length': 5}}
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
         """Create and return user with encrypted password"""
