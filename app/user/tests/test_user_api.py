3a7e2b2adbf4158867ff8cb91022017979366edb
ValyT <40295158+ValyT@users.noreply.github.com>
2023-08-28 12:12:28 +0300

    auth token api functionality + flake8 stuff

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -10,6 +10,7 @@ from rest_framework import status
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
 
+
 def create_user(**params):
     """Create and return new user"""
     return get_user_model().objects.create_user(**params)
@@ -27,7 +28,7 @@ class PublicUserApiTests(TestCase):
             'password': 'testpass123',
             'name': 'Test Name',
         }
-        res=self.client.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
@@ -42,7 +43,7 @@ class PublicUserApiTests(TestCase):
             'name': 'Test Name',
         }
         create_user(**payload)
-        res=self.client.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
@@ -53,56 +54,57 @@ class PublicUserApiTests(TestCase):
             'password': 'tp12',
             'name': 'Test Name',
         }
-        res=self.client.post(CREATE_USER_URL, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
         user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
         self.assertFalse(user_exists)
+
     def test_create_user_token(self):
-            """Test generate access token for valid credentials"""
-            user_details = {
-                'email': 'test@example.com',
-                'password': 'test-user-pass123',
-                'name': 'Test Name',
-            }
+        """Test generate access token for valid credentials"""
+        user_details = {
+            'email': 'test@example.com',
+            'password': 'test-user-pass123',
+            'name': 'Test Name',
+        }
 
-            create_user(**user_details)
+        create_user(**user_details)
 
-            payload = {
+        payload = {
             'email': user_details['email'],
             'password': user_details['password'],
-            }
+        }
 
-            res=self.client.post(TOKEN_URL, payload)
+        res = self.client.post(TOKEN_URL, payload)
 
-            self.assertIn('token', res.data)
-            self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
 
     def test_create_token_bad_credentials(self):
-            """Test returns error for invalid credentials"""
-            create_user(email='test@example.com', password='goodpass')
+        """Test returns error for invalid credentials"""
+        create_user(email='test@example.com', password='goodpass')
 
-            payload = {
+        payload = {
             'email': 'test@example.com',
-            'password': 'badpass'
-            }
+            'password': 'badpass',
+        }
 
-            res=self.client.post(TOKEN_URL, payload)
+        res = self.client.post(TOKEN_URL, payload)
 
-            self.assertNotIn('token', res.data)
-            self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_blank_password(self):
-            """Test returns error for missing credentials"""
+        """Test returns error for missing credentials"""
 
-            payload = {
+        payload = {
             'email': 'test@example.com',
-            'password': ''
-            }
+            'password': '',
+        }
 
-            res=self.client.post(TOKEN_URL, payload)
+        res = self.client.post(TOKEN_URL, payload)
 
-            self.assertNotIn('token', res.data)
-            self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/user/views.py
+++ app/user/views.py
@@ -1,10 +1,20 @@
 """Views for user API"""
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create new user in system"""
-    serializer_class=UserSerializer
+    serializer_class = UserSerializer
 
+
+class CreateTokenView(ObtainAuthToken):
+    """Createauth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
