dc3c84f433af7410510c6c682349ed48f452e53a
ValyT <40295158+ValyT@users.noreply.github.com>
2023-08-31 10:02:41 +0200

    tests for recipe filtering

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -399,6 +399,48 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(recipe.ingredients.count(), 0)
 
+    def test_filter_by_tags(self):
+        """Test tag filtering"""
+        r1 = create_recipe(user=self.user, title='Thai Vegetebale Soup')
+        r2 = create_recipe(user=self.user, title='Tahini Aubergine')
+        r3 = create_recipe(user=self.user, title='Fish and Chips')
+        tag1 = Tag.objects.create(user=self.user, name='Vegan')
+        tag2 = Tag.objects.create(user=self.user, name='Vegetarian')
+        r1.tags.add(tag1)
+        r2.tags.add(tag2)
+
+        params = {'tags': f'{tag1.id},{tag2.id}'}
+        res = self.client.get(RECIPES_URL, params)
+
+        s1=RecipeSerializer(r1)
+        s2=RecipeSerializer(r2)
+        s3=RecipeSerializer(r3)
+
+        self.assertIn(s1.data, res.data)
+        self.assertIn(s2.data, res.data)
+        self.assertNotIn(s3.data, res.data)
+
+    def test_filter_by_ingredients(self):
+        """Test ingredients filtering"""
+        r1 = create_recipe(user=self.user, title='Posh Beans on Toast')
+        r2 = create_recipe(user=self.user, title='Chicken Cacciatore')
+        r3 = create_recipe(user=self.user, title='Red Lentil Daal')
+        ing1 = Ingredient.objects.create(user=self.user, name='Feta Cheese')
+        ing2 = Ingredient.objects.create(user=self.user, name='Chicken')
+        r1.ingredients.add(ing1)
+        r2.ingredients.add(ing2)
+
+        params = {'ingredients': f'{ing1.id},{ing2.id}'}
+        res = self.client.get(RECIPES_URL, params)
+
+        s1=RecipeSerializer(r1)
+        s2=RecipeSerializer(r2)
+        s3=RecipeSerializer(r3)
+
+        self.assertIn(s1.data, res.data)
+        self.assertIn(s2.data, res.data)
+        self.assertNotIn(s3.data, res.data)
+
 
 class ImageUploadTests(TestCase):
     """Tests for image upload API"""
