a8b6333bb52f8f438d714773ef44bad6b5996c44
ValyT <40295158+ValyT@users.noreply.github.com>
2023-08-31 14:30:06 +0200

    django settings updated+ typo fixes

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ztv&&*sylte@(c&1o)j&-f(h$wp1q)#^ry@x3-3l!9_jg31yq9'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTs', '').split(','),
+    )
+)
 
 
 # Application definition
